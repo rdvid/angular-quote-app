df32e89292408e0628ead0e3ef41f9c4980c51b8
Rafo <rafael_dvid@hotmail.com>
2023-02-09 19:20:16 -0300

    creating header and footer, creating quote component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CreateQuotesComponent } from './components/quotes/create-quotes/create-quotes.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, HeaderComponent],
+  declarations: [AppComponent, HomeComponent, HeaderComponent, FooterComponent, CreateQuotesComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/components/quotes/create-quotes/create-quotes.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-quotes',
+  templateUrl: './create-quotes.component.html',
+  styleUrls: ['./create-quotes.component.css']
+})
+export class CreateQuotesComponent {
+
+}
